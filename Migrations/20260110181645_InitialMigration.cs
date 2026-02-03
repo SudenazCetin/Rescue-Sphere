ed3e93234027e9eeb01bda0249d4c1f9440f9fac
SudenazCetin <sudenazcetin77@gmail.com>
2026-02-04 00:35:04 +0300

    readme güncellendi

--- Migrations/20260110181645_InitialMigration.cs
+++ Migrations/20260110181645_InitialMigration.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
-namespace Rescue_Sphere.Migrations
+namespace RescueSphere.Api.Migrations
 {
     /// <inheritdoc />
     public partial class InitialMigration : Migration
